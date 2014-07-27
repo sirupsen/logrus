ce7430e83f8bf40eba5ea1b80c5bf426690d6d12
Evan Huus <eapache@gmail.com>
2014-07-27 17:51:33 -0400

    If an airbrake hook got no 'error' field, try the first arg
    This simplifies cases where all you want to do is log the error without any
    additional message, since you can now do `.Debug(err)` instead of
    `.WithFields(...).Debug(???)`.

--- hooks/airbrake/airbrake.go
+++ hooks/airbrake/airbrake.go
@@ -16,20 +16,15 @@ import (
 type AirbrakeHook struct{}
 
 func (hook *AirbrakeHook) Fire(entry *logrus.Entry) error {
-	if entry.Data["error"] == nil {
-		entry.Logger.WithFields(logrus.Fields{
-			"source":   "airbrake",
-			"endpoint": airbrake.Endpoint,
-		}).Warn("Exceptions sent to Airbrake must have an 'error' key with the error")
-		return nil
+	errVal, ok := entry.Data["error"].(error)
+	if (!ok || errVal == nil) && len(entry.Args >= 1) {
+		errVal, ok = entry.Args[0].(error)
 	}
-
-	err, ok := entry.Data["error"].(error)
-	if !ok {
+	if !ok || errVal == nil {
 		entry.Logger.WithFields(logrus.Fields{
 			"source":   "airbrake",
 			"endpoint": airbrake.Endpoint,
-		}).Warn("Exceptions sent to Airbrake must have an `error` key of type `error`")
+		}).Warn("Exceptions sent to Airbrake must have an 'error' key or message of type `error`")
 		return nil
 	}
 
