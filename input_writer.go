299ee952775f50ef63047af52f17a034d7da634b
Patrick Hemmer <patrick.hemmer@gmail.com>
2015-01-08 01:04:07 -0500

    add io.Writer interface compatability

---
+++ input_writer.go
@@ -0,0 +1,30 @@
+package logrus
+
+import (
+	"bufio"
+	"io"
+	"runtime"
+)
+
+func (logger *Logger) InputWriter() (*io.PipeWriter) {
+	inputReader, inputWriter := io.Pipe()
+
+	go logger.inputWriterScanner(inputReader)
+	runtime.SetFinalizer(inputWriter, inputWriterFinalizer)
+
+	return inputWriter
+}
+
+func (logger *Logger) inputWriterScanner(inputReader *io.PipeReader) {
+	scanner := bufio.NewScanner(inputReader)
+	for scanner.Scan() {
+		logger.Print(scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		logger.Fatal(err)
+	}
+}
+
+func inputWriterFinalizer(inputWriter *io.PipeWriter) {
+	inputWriter.Close()
+}
