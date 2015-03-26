925916cf77c0c7d7d9266098b0c97673438e0c2b
Philippe Lafoucrière <philippe.lafoucriere@tech-angels.com>
2015-03-26 16:05:11 -0400

    Use a buffered channel instead of spawing
    The fire() func is spawned only once and will loop over a buffered
    channel. The channel size can be set using graylog.BufSize, but the
    default 1024 should be more than enough.
    
    Remember the call to Fire will start to block if the channel is full
    (airbrake doesn't respond).
    Please not that tobi/airbrake-go *never* times out on sending the error
    to airbrake, so I think this is a timebomb.
    The official github.com/airbrake/gobrake has a default timeout of 10s,
    which is a lot better I think.

--- hooks/airbrake/airbrake.go
+++ hooks/airbrake/airbrake.go
@@ -3,10 +3,14 @@ package airbrake
 import (
 	"errors"
 	"fmt"
+	"os"
 
 	"github.com/Sirupsen/logrus"
 	"github.com/tobi/airbrake-go"
 )
+
+// Set graylog.BufSize = <value> _before_ calling NewHook
+var BufSize uint = 1024
 
 // AirbrakeHook to send exceptions to an exception-tracking service compatible
 // with the Airbrake API.
@@ -14,40 +18,46 @@ type airbrakeHook struct {
 	APIKey      string
 	Endpoint    string
 	Environment string
+	entryChan   chan *logrus.Entry
 }
 
 func NewHook(endpoint, apiKey, env string) *airbrakeHook {
-	return &airbrakeHook{
+	hook := &airbrakeHook{
 		APIKey:      apiKey,
 		Endpoint:    endpoint,
 		Environment: env,
+		entryChan:   make(chan *logrus.Entry, BufSize),
 	}
+	go hook.fire()
+	return hook
 }
 
 func (hook *airbrakeHook) Fire(entry *logrus.Entry) error {
-	go hook.fire(entry) // Don't block logging
+	hook.entryChan <- entry
 	return nil
 }
 
-func (hook *airbrakeHook) fire(entry *logrus.Entry) error {
-	airbrake.ApiKey = hook.APIKey
-	airbrake.Endpoint = hook.Endpoint
-	airbrake.Environment = hook.Environment
+// fire sends errors to airbrake when an entry is available on entryChan
+func (hook *airbrakeHook) fire() {
+	for {
+		entry := <-hook.entryChan
+		airbrake.ApiKey = hook.APIKey
+		airbrake.Endpoint = hook.Endpoint
+		airbrake.Environment = hook.Environment
 
-	var notifyErr error
-	err, ok := entry.Data["error"].(error)
-	if ok {
-		notifyErr = err
-	} else {
-		notifyErr = errors.New(entry.Message)
+		var notifyErr error
+		err, ok := entry.Data["error"].(error)
+		if ok {
+			notifyErr = err
+		} else {
+			notifyErr = errors.New(entry.Message)
+		}
+
+		if err = airbrake.Notify(notifyErr); err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to send error to Airbrake: %v\n", err)
+		}
+
 	}
-
-	airErr := airbrake.Notify(notifyErr)
-	if airErr != nil {
-		return fmt.Errorf("Failed to send error to Airbrake: %s", airErr)
-	}
-
-	return nil
 }
 
 func (hook *airbrakeHook) Levels() []logrus.Level {
