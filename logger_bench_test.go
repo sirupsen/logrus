bc35b026f0f9469fbd784f9066e936722d144c27
plan <plan@plan-tp>
2016-06-19 03:21:15 +0800

    Provide logger.SetNoLock() to remove locking during log output
    Locking is enabled by default. When file is opened with appending mode,
    it's safe to write concurrently to a file. In this case user can
    choose to disable the lock.

--- logger_bench_test.go
+++ logger_bench_test.go
@@ -22,12 +22,36 @@ func BenchmarkDummyLogger(b *testing.B) {
 	doLoggerBenchmark(b, nullf, &TextFormatter{DisableColors: true}, smallFields)
 }
 
+func BenchmarkDummyLoggerNoLock(b *testing.B) {
+	nullf, err := os.OpenFile("/dev/null", os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		b.Fatalf("%v", err)
+	}
+	defer nullf.Close()
+	doLoggerBenchmarkNoLock(b, nullf, &TextFormatter{DisableColors: true}, smallFields)
+}
+
 func doLoggerBenchmark(b *testing.B, out *os.File, formatter Formatter, fields Fields) {
 	logger := Logger{
 		Out:       out,
 		Level:     InfoLevel,
 		Formatter: formatter,
 	}
+	entry := logger.WithFields(fields)
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			entry.Info("aaa")
+		}
+	})
+}
+
+func doLoggerBenchmarkNoLock(b *testing.B, out *os.File, formatter Formatter, fields Fields) {
+	logger := Logger{
+		Out:       out,
+		Level:     InfoLevel,
+		Formatter: formatter,
+	}
+	logger.SetNoLock()
 	entry := logger.WithFields(fields)
 	b.RunParallel(func(pb *testing.PB) {
 		for pb.Next() {
