d0b91bafa9f6f25f524ddc58deb2304a6af59366
a.guzev <aguzev@sfxdx.ru>
2019-01-08 20:52:26 +1000

    hooks/syslog stub for syslog-less systems

---
+++ hooks/syslog/stub.go
@@ -0,0 +1,24 @@
+// +build windows,nacl,plan9
+
+package syslog
+
+import (
+	"github.com/sirupsen/logrus"
+)
+
+// SyslogHook to do nothing on syslogless systems.
+type SyslogHook struct {
+}
+
+// Creates a stub hook to be added to an instance of logger on syslogless systems.
+func NewSyslogHook(network, raddr string, priority int, tag string) (*SyslogHook, error) {
+	return &SyslogHook{}, nil
+}
+
+func (hook *SyslogHook) Fire(entry *logrus.Entry) error {
+	return nil
+}
+
+func (hook *SyslogHook) Levels() []logrus.Level {
+	return make([]logrus.Level, 0, 0)
+}
