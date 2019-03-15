d7449938e4b151a5274efc6b5f859fedca927d5e
tbunyk <tbunyk@gmail.com>
2019-03-15 13:09:11 +0200

    Add hook to send logs to custom writer #678

---
+++ hooks/writer/writer_test.go
@@ -0,0 +1,37 @@
+package writer
+
+import (
+	"bytes"
+	"io/ioutil"
+	"testing"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDifferentLevelsGoToDifferentWriters(t *testing.T) {
+	var a, b bytes.Buffer
+
+	log.SetFormatter(&log.TextFormatter{
+		DisableTimestamp: true,
+	})
+	log.SetOutput(ioutil.Discard) // Send all logs to nowhere by default
+
+	log.AddHook(&Hook{
+		Writer: &a,
+		LogLevels: []log.Level{
+			log.WarnLevel,
+		},
+	})
+	log.AddHook(&Hook{ // Send info and debug logs to stdout
+		Writer: &b,
+		LogLevels: []log.Level{
+			log.InfoLevel,
+		},
+	})
+	log.Warn("send to a")
+	log.Info("send to b")
+
+	assert.Equal(t, a.String(), "level=warning msg=\"send to a\"\n")
+	assert.Equal(t, b.String(), "level=info msg=\"send to b\"\n")
+}
