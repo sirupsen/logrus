0775a3c4e9d1646002d4b46e572a01bf67af49e8
Sebastien Lavoie <sebastien.lavoie@shopify.com>
2019-03-26 11:33:52 -0400

    Inline the use of IsTerminal to remove crypto dependency
    golang.org/x/crypto is quite a big package

--- terminal_check_notappengine.go
+++ terminal_check_notappengine.go
@@ -6,13 +6,16 @@ import (
 	"io"
 	"os"
 
-	"golang.org/x/crypto/ssh/terminal"
+	"golang.org/x/sys/unix"
 )
 
 func checkIfTerminal(w io.Writer) bool {
 	switch v := w.(type) {
 	case *os.File:
-		return terminal.IsTerminal(int(v.Fd()))
+		// Returns whether the given file descriptor is a terminal.
+		// Taken from golang.org/x/crypto/ssh/terminal.IsTerminal without importing the whole package
+		_, err := unix.IoctlGetTermios(int(v.Fd()), unix.TIOCGETA)
+		return err == nil
 	default:
 		return false
 	}
