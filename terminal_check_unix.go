003af98bf096fc7575feabaff2878e4e913c1ec1
Andrey Tcherepanov <xwa8wnqa3a@snkmail.com>
2019-04-01 10:16:11 -0600

    Make isTerminal un-exported

--- terminal_check_unix.go
+++ terminal_check_unix.go
@@ -1,12 +1,12 @@
 // +build linux aix
 
-package terminal
+package logrus
 
 import "golang.org/x/sys/unix"
 
 const ioctlReadTermios = unix.TCGETS
 
-func IsTerminal(fd int) bool {
+func isTerminal(fd int) bool {
 	_, err := unix.IoctlGetTermios(fd, ioctlReadTermios)
 	return err == nil
 }
