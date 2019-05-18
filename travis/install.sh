13888978d49d266c050841fe96f2923a16111aec
David Bariod <davidriod@googlemail.com>
2019-05-18 10:01:53 +0200

    Only perform the full cross-compile on single build

--- travis/install.sh
+++ travis/install.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-if [[ "$TRAVIS_GO_VERSION" =~ ^1.\12\. ]]; then
+if [[ "$TRAVIS_GO_VERSION" =~ ^1.\12\. ]] && [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     git clone https://github.com/dgsb/gox.git /tmp/gox
     pushd /tmp/gox
     go build ./
