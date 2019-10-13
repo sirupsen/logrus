75440f2ebeaade97af7e2cdc272dc9fde03f1f5b
Edward Muller <emuller@salesforce.com>
2019-10-13 16:51:57 -0700

    get some other deps

--- travis/cross_build.sh
+++ travis/cross_build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-if [[ "$TRAVIS_GO_VERSION" =~ ^1.\12\. ]] && [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+if [[ "$TRAVIS_GO_VERSION" =~ ^1\.12\. ]] && [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     /tmp/gox/gox -build-lib -all
 fi
