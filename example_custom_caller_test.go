b8701320fe4733693e8dce14b06bb6186c740805
Edward Muller <freeformz@gmail.com>
2019-10-14 13:23:44 -0700

    fix broken test

--- example_custom_caller_test.go
+++ example_custom_caller_test.go
@@ -24,5 +24,5 @@ func ExampleJSONFormatter_CallerPrettyfier() {
 	}
 	l.Info("example of custom format caller")
 	// Output:
-	// {"file":"example_custom_caller_test.go","func":"ExampleCustomFormatter","level":"info","msg":"example of custom format caller"}
+	// {"file":"example_custom_caller_test.go","func":"ExampleJSONFormatter_CallerPrettyfier","level":"info","msg":"example of custom format caller"}
 }
