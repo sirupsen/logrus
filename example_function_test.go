ba4da53cffb0ed6b42e5cfa334a011e55448f7db
David Bariod <davidriod@googlemail.com>
2020-05-19 17:02:33 +0200

    Improve tests for logger.*Fn functions

--- example_function_test.go
+++ example_function_test.go
@@ -1,26 +1,31 @@
 package logrus_test
 
 import (
-	"fmt"
-	log "github.com/sirupsen/logrus"
 	"testing"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestLogger_LogFn(t *testing.T) {
 	log.SetFormatter(&log.JSONFormatter{})
 	log.SetLevel(log.WarnLevel)
 
+	notCalled := 0
 	log.InfoFn(func() []interface{} {
-		fmt.Println("This is never run")
-		return []interface{} {
+		notCalled++
+		return []interface{}{
 			"Hello",
 		}
 	})
+	assert.Equal(t, 0, notCalled)
 
+	called := 0
 	log.ErrorFn(func() []interface{} {
-		fmt.Println("This runs")
-		return []interface{} {
+		called++
+		return []interface{}{
 			"Oopsi",
 		}
 	})
+	assert.Equal(t, 1, called)
 }
