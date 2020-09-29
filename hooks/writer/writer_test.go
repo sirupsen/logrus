47904d71d78d0a54c55208e43e067a098706718d
Sivachandran <sivachandran@qubecinema.com>
2020-09-29 14:18:34 +0530

    Add formatter to writer hook

--- hooks/writer/writer_test.go
+++ hooks/writer/writer_test.go
@@ -36,3 +36,41 @@ func TestDifferentLevelsGoToDifferentWriters(t *testing.T) {
 	assert.Equal(t, a.String(), "level=warning msg=\"send to a\"\n")
 	assert.Equal(t, b.String(), "level=info msg=\"send to b\"\n")
 }
+
+func TestDifferentFormattersToDifferentWritter(t *testing.T) {
+	var a, b bytes.Buffer
+
+	log.SetOutput(ioutil.Discard) // Send all logs to nowhere by default
+
+	log.AddHook(&Hook{
+		Writer: &a,
+		LogLevels: []log.Level{
+			log.WarnLevel,
+		},
+		Formatter: &log.TextFormatter{
+			DisableTimestamp: true,
+			DisableColors:    true,
+			FieldMap: log.FieldMap{
+				log.FieldKeyLevel: "@level",
+				log.FieldKeyMsg:   "@message",
+			},
+		},
+	})
+	log.AddHook(&Hook{ // Send info and debug logs to stdout
+		Writer: &b,
+		LogLevels: []log.Level{
+			log.InfoLevel,
+		},
+		Formatter: &log.JSONFormatter{
+			DisableTimestamp: true,
+			FieldMap: log.FieldMap{
+				log.FieldKeyMsg: "message",
+			},
+		},
+	})
+	log.Warn("send to a")
+	log.Info("send to b")
+
+	assert.Equal(t, a.String(), "@level=warning @message=\"send to a\"\n")
+	assert.Equal(t, b.String(), "{\"level\":\"info\",\"message\":\"send to b\"}\n")
+}
