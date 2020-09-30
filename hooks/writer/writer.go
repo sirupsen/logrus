08a0f857f24abcaddfbf838ed3a3c926e83194bc
mycaosf <mycaosf@gmail.com>
2020-09-30 17:10:17 +0800

    modify syslog, test, writer's logrus address

--- hooks/writer/writer.go
+++ hooks/writer/writer.go
@@ -3,7 +3,7 @@ package writer
 import (
 	"io"
 
-	log "github.com/sirupsen/logrus"
+	log "github.com/mycaosf/logrus"
 )
 
 // Hook is a hook that writes logs of specified LogLevels to specified Writer
