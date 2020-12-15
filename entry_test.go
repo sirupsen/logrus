02fcb160055b97acc1381d9d88cb36e74ce2a535
Alec Benzer <alec@level.com>
2020-12-15 18:25:34 -0500

    Remove dead panic in Entry.Panic
    [Entry.log itself panics][0] when the log level is set to PanicLevel, (and
    PanicLevel is always eneabled) so this second panic will never be reached.
    
    [0]: https://github.com/sirupsen/logrus/blob/8ae478eb8a850a54ea4915a2b572f377de1f9c7e/entry.go#L253

--- entry_test.go
+++ entry_test.go
@@ -167,6 +167,28 @@ func TestEntryPanicf(t *testing.T) {
 	entry.WithField("err", errBoom).Panicf("kaboom %v", true)
 }
 
+func TestEntryPanic(t *testing.T) {
+	errBoom := fmt.Errorf("boom again")
+
+	defer func() {
+		p := recover()
+		assert.NotNil(t, p)
+
+		switch pVal := p.(type) {
+		case *Entry:
+			assert.Equal(t, "kaboom", pVal.Message)
+			assert.Equal(t, errBoom, pVal.Data["err"])
+		default:
+			t.Fatalf("want type *Entry, got %T: %#v", pVal, pVal)
+		}
+	}()
+
+	logger := New()
+	logger.Out = &bytes.Buffer{}
+	entry := NewEntry(logger)
+	entry.WithField("err", errBoom).Panic("kaboom")
+}
+
 const (
 	badMessage   = "this is going to panic"
 	panicMessage = "this is broken"
