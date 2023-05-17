39be60d7af5d2144ea004aedc3db8cb68a92ec42
Paul Holzinger <pholzing@redhat.com>
2023-05-17 15:39:49 +0200

    fix panic in Writer
    Commit 766cfece introduced this bug by defining an incorrect split
    function. First it breaks the old behavior because it never splits at
    newlines now. Second, it causes a panic because it never tells the
    scanner to stop. See the bufio.ScanLines function, something like:
    ```
    if atEOF && len(data) == 0 {
    	return 0, nil, nil
    }
    ```
    is needed to do that.
    
    This commit fixes it by restoring the old behavior and calling
    bufio.ScanLines but also keep the 64KB check in place to avoid buffering
    for to long.
    
    Two tests are added to ensure it is working as expected.
    
    Fixes #1383
    
    Signed-off-by: Paul Holzinger <pholzing@redhat.com>

--- writer.go
+++ writer.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"io"
 	"runtime"
+	"strings"
 )
 
 // Writer at INFO level. See WriterLevel for details.
@@ -54,14 +55,37 @@ func (entry *Entry) WriterLevel(level Level) *io.PipeWriter {
 	return writer
 }
 
+// writerScanner scans the input from the reader and writes it to the logger
 func (entry *Entry) writerScanner(reader *io.PipeReader, printFunc func(args ...interface{})) {
 	scanner := bufio.NewScanner(reader)
-	for scanner.Scan() {
-		printFunc(scanner.Text())
+
+	// Set the buffer size to the maximum token size to avoid buffer overflows
+	scanner.Buffer(make([]byte, bufio.MaxScanTokenSize), bufio.MaxScanTokenSize)
+
+	// Define a split function to split the input into chunks of up to 64KB
+	chunkSize := bufio.MaxScanTokenSize // 64KB
+	splitFunc := func(data []byte, atEOF bool) (int, []byte, error) {
+		if len(data) >= chunkSize {
+			return chunkSize, data[:chunkSize], nil
+		}
+
+		return bufio.ScanLines(data, atEOF)
 	}
+
+	// Use the custom split function to split the input
+	scanner.Split(splitFunc)
+
+	// Scan the input and write it to the logger using the specified print function
+	for scanner.Scan() {
+		printFunc(strings.TrimRight(scanner.Text(), "\r\n"))
+	}
+
+	// If there was an error while scanning the input, log an error
 	if err := scanner.Err(); err != nil {
 		entry.Errorf("Error while reading from Writer: %s", err)
 	}
+
+	// Close the reader when we are done
 	reader.Close()
 }
 
