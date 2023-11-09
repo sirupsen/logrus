4e3d3133d57894562639ccc807bcb1d7fd3466fc
Jonathan Hall <flimzy@flimzy.com>
2023-11-09 09:59:13 +0100

    Basic slog hook
    Co-authored-by: Jonathan Hall <flimzy@flimzy.com>
    Co-authored-by: Sebastiaan van Stijn <github@gone.nl>
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

---
+++ hooks/slog/slog.go
@@ -0,0 +1,73 @@
+package slog
+
+import (
+	"context"
+	"log/slog"
+
+	"github.com/sirupsen/logrus"
+)
+
+// SlogHook sends logs to slog.
+type SlogHook struct {
+	logger *slog.Logger
+}
+
+var _ logrus.Hook = (*SlogHook)(nil)
+
+// NewSlogHook creates a hook that sends logs to an existing slog Logger.
+// This hook is intended to be used during transition from Logrus to slog,
+// or as a shim between different parts of your application or different
+// libraries that depend on different loggers.
+//
+// The provided logger must not be nil. NewSlogHook panics if logger is nil.
+//
+// Example usage:
+//
+//	logger := slog.New(slog.NewJSONHandler(os.Stderr, nil))
+//	hook := NewSlogHook(logger)
+func NewSlogHook(logger *slog.Logger) *SlogHook {
+	if logger == nil {
+		panic("cannot create hook from nil logger")
+	}
+	return &SlogHook{
+		logger: logger,
+	}
+}
+
+func (*SlogHook) toSlogLevel(level logrus.Level) slog.Level {
+	switch level {
+	case logrus.PanicLevel, logrus.FatalLevel, logrus.ErrorLevel:
+		return slog.LevelError
+	case logrus.WarnLevel:
+		return slog.LevelWarn
+	case logrus.InfoLevel:
+		return slog.LevelInfo
+	case logrus.DebugLevel, logrus.TraceLevel:
+		return slog.LevelDebug
+	default:
+		// Treat all unknown levels as errors
+		return slog.LevelError
+	}
+}
+
+// Levels always returns all levels, since slog allows controlling level
+// enabling based on context.
+func (h *SlogHook) Levels() []logrus.Level {
+	return logrus.AllLevels
+}
+
+// Fire sends entry to the underlying slog logger. The Time and Caller fields
+// of entry are ignored.
+func (h *SlogHook) Fire(entry *logrus.Entry) error {
+	ctx := entry.Context
+	if ctx == nil {
+		ctx = context.Background()
+	}
+	lvl := h.toSlogLevel(entry.Level)
+	attrs := make([]any, 0, len(entry.Data))
+	for k, v := range entry.Data {
+		attrs = append(attrs, slog.Any(k, v))
+	}
+	h.logger.Log(ctx, lvl, entry.Message, attrs...)
+	return nil
+}
