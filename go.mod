7f28b9b6ec5776a4d19118dad31450f85920ddb8
Sebastiaan van Stijn <github@gone.nl>
2026-02-24 12:26:46 +0100

    use golang.org/x/term for terminal detection
    Using a low version of this dependency, as the `IsTerminal` function
    was not modified in newer versions;
    
    https://github.com/golang/term/compare/v0.2.0...v0.40.0
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.23
 require (
 	github.com/stretchr/testify v1.10.0
 	golang.org/x/sys v0.13.0
+	golang.org/x/term v0.2.0
 )
 
 require (
--- terminal_check_windows.go
+++ terminal_check_windows.go
@@ -7,21 +7,32 @@ import (
 	"os"
 
 	"golang.org/x/sys/windows"
+	"golang.org/x/term"
 )
 
 func checkIfTerminal(w io.Writer) bool {
-	switch v := w.(type) {
-	case *os.File:
-		handle := windows.Handle(v.Fd())
-		var mode uint32
-		if err := windows.GetConsoleMode(handle, &mode); err != nil {
+	if f, ok := w.(*os.File); ok {
+		fd := f.Fd()
+		maxInt := uintptr(^uint(0) >> 1)
+		if fd > maxInt {
 			return false
+		}
+		if !term.IsTerminal(int(f.Fd())) {
+			return false
+		}
+
+		h := windows.Handle(f.Fd())
+		var mode uint32
+		if err := windows.GetConsoleMode(h, &mode); err != nil {
+			return false
+		}
+		if mode&windows.ENABLE_VIRTUAL_TERMINAL_PROCESSING != 0 {
+			return true
 		}
 		mode |= windows.ENABLE_VIRTUAL_TERMINAL_PROCESSING
-		if err := windows.SetConsoleMode(handle, mode); err != nil {
+		if err := windows.SetConsoleMode(h, mode); err != nil {
 			return false
 		}
-		return true
 	}
 	return false
 }
