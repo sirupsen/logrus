61323e1f35a9064c144ece4c6b56163dbb8aa776
Sebastiaan van Stijn <github@gone.nl>
2026-03-02 21:25:59 +0100

    entry: add BenchmarkEntry_WithFields
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- entry_bench_test.go
+++ entry_bench_test.go
@@ -35,3 +35,51 @@ func BenchmarkEntry_WithField_Chain(b *testing.B) {
 		_ = e
 	}
 }
+
+func BenchmarkEntry_WithFields(b *testing.B) {
+	fn := func() {}
+	fnPtr := &fn
+
+	tests := []struct {
+		name string
+		base logrus.Fields
+		fields logrus.Fields
+	}{
+		{
+			name:   "valid_fields_only",
+			base:   logrus.Fields{"a": 1, "b": "two"},
+			fields: logrus.Fields{"c": 3, "d": "four"},
+		},
+		{
+			name:   "contains_func",
+			base:   logrus.Fields{"a": 1},
+			fields: logrus.Fields{"bad": fn},
+		},
+		{
+			name:   "contains_func_ptr",
+			base:   logrus.Fields{"a": 1},
+			fields: logrus.Fields{"bad": fnPtr},
+		},
+		{
+			name:   "mixed_valid_invalid",
+			base:   logrus.Fields{"a": 1, "b": 2},
+			fields: logrus.Fields{"c": 3, "bad": fn, "d": 4},
+		},
+		{
+			name:   "larger_map",
+			base:   logrus.Fields{"a": 1, "b": 2, "c": 3, "d": 4, "e": 5, "f": 6, "g": 7, "h": 8, "i": 9, "j": 10},
+			fields: logrus.Fields{"k": 11, "l": 12, "m": 13, "n": 14, "o": 15},
+		},
+	}
+
+	for _, tc := range tests {
+		b.Run(tc.name, func(b *testing.B) {
+			b.ReportAllocs()
+			e := &logrus.Entry{Data: tc.base}
+			b.ResetTimer()
+			for i := 0; i < b.N; i++ {
+				_ = e.WithFields(tc.fields)
+			}
+		})
+	}
+}
