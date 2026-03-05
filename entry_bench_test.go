17068fbdc2aafafe58f50b3f08ab5946ffacdf90
Sebastiaan van Stijn <github@gone.nl>
2026-03-05 21:16:47 +0100

    entry: add BenchmarkEntry_WithField_Chain
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- entry_bench_test.go
+++ entry_bench_test.go
@@ -18,3 +18,20 @@ func BenchmarkEntry_WithError(b *testing.B) {
 	}
 }
 
+func BenchmarkEntry_WithField_Chain(b *testing.B) {
+	base := &logrus.Entry{Data: logrus.Fields{"a": 1}}
+	errBoom := errors.New("boom")
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		e := base
+
+		e = e.WithField("k0", 0)
+		e = e.WithField("k1", 1)
+		e = e.WithField("k2", 2)
+		e = e.WithField("k3", 3)
+		e = e.WithError(errBoom)
+		_ = e
+	}
+}
